f1fee4a3bd320280801dc13ba0d0236a40f23408
stefano marchisio <stamarch@virgilio.it>
2022-11-12 00:07:02 +0100

    prova6

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/app-test1'),
+      dir: require('path').join(__dirname, './coverage/AppNgxDatatableDynamic'),
       subdir: '.',
       reporters: [
         { type: 'html' },
